21d397f42a9357d7ecb906e75045e99ffa9f8de8
Chris Smith <chris@cs278.org>
2009-07-10 23:11:17 +0100

    {c,m}time for ideas

--- install/mysql.sql
+++ install/mysql.sql
@@ -9,6 +9,8 @@ CREATE TABLE phpbb_np_ideas (
 	vote_cost smallint(4) NOT NULL,
 	vote_closed tinyint(1) DEFAULT 0 NOT NULL,
 	rejected tinyint(1) DEFAULT 0 NOT NULL,
+	ctime int(11) UNSIGNED NOT NULL,
+	mtime int(11) UNSIGNED NOT NULL,
 	PRIMARY KEY (id),
 	KEY user_id (user_id)
 ) CHARACTER SET `utf8` COLLATE `utf8_bin`;
@@ -20,8 +22,8 @@ CREATE TABLE phpbb_np_votes (
 	`count` smallint(4) NOT NULL,
 	`value` smallint(4) NOT NULL,
 	cost smallint(4) NOT NULL,
-	ctime int(11) NOT NULL,
-	mtime int(11) NOT NULL,
+	ctime int(11) UNSIGNED NOT NULL,
+	mtime int(11) UNSIGNED NOT NULL,
 	PRIMARY KEY (id),
 	UNIQUE idea_user (idea_id, user_id)
 ) CHARACTER SET `utf8` COLLATE `utf8_bin`;
