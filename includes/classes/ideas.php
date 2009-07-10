ab613386e5db8f48a902a5cb26e44667510c9e1c
Chris Smith <chris@cs278.org>
2009-07-10 01:54:57 +0100

    best add this file

---
+++ includes/classes/ideas.php
@@ -0,0 +1,16 @@
+<?php
+
+// For lack of a better name currently -_-
+
+class ideas
+{
+	public static function autoload($class)
+	{
+		if (!preg_match('#^[a-z][a-z0-9_]+$#', $class))
+		{
+			return;
+		}
+
+		require IDEAS_ROOT_PATH . 'includes/classes/' . $class . '.php';
+	}
+}
