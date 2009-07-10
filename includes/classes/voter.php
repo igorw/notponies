64014cd5febc22883c5f0bb307c752c4d2b335a4
Chris Smith <chris@cs278.org>
2009-07-10 23:38:12 +0100

    add shortcut methods

--- includes/classes/voter.php
+++ includes/classes/voter.php
@@ -3,6 +3,15 @@
 class voter extends np_record
 {
 	private $name;
+	private $colour;
+	private $rank;
+	private $posts;
+	private $avatar = array(
+		'file'	=> '',
+		'type'	=> 0,
+		'width'	=> 0,
+		'height'=> 0,
+	);
 
 	private $base_points;
 
@@ -17,11 +26,51 @@ class voter extends np_record
 		np_registry::get_instance()->register($this);
 
 		$this->id			= (int) $data['id'];
-		$this->name			= $data['name'];
+		$this->name			= $data['username'];
+		$this->colour		= $data['user_colour'];
+		$this->rank			= (int) $data['user_rank'];
+		$this->posts		= (int) $data['user_posts'];
+		$this->avatar		= array(
+			'file'	=> $data['user_avatar'],
+			'type'	=> (int) $data['user_avatar_type'],
+			'width'	=> (int) $data['user_avatar_width'],
+			'height'=> (int) $data['user_avatar_height'],
+		);
 		$this->base_points	= (int) $data['base_points'];
 
 		// Calculate on request
 		$this->points = null;
+	}
+
+	public function rank()
+	{
+		if (!function_exists('get_user_rank'))
+		{
+			global $phpbb_root_path, $phpEx;
+
+			include $phpbb_root_path . 'includes/functions_display.' . $phpEx;
+		}
+
+		$return = array(
+			'title'		=> '',
+			'img'		=> '',
+			'img_src'	=> '',
+		);
+
+		get_user_rank($this->rank, $this->posts, $return['title'], $return['img'], $return['img_src']);
+
+		return $return;
+	}
+
+	public function avatar($alt = 'USER_AVATAR', $ignore_config = false)
+	{
+		if (!function_exists('get_user_avatar'))
+		{
+			global $phpbb_root_path, $phpEx;
+
+			include $phpbb_root_path . 'includes/functions_display.' . $phpEx;
+		}
+		return get_user_avatar($this->avatar['file'], $this->avatar['type'], $this->avatar['width'], $this->avatar['height'], $alt, $ignore_config);
 	}
 
 	public static function get_current()
@@ -35,7 +84,8 @@ class voter extends np_record
 	{
 		global $db;
 
-		$sql = 'SELECT v.*, u.user_id, u.username AS name
+		$sql = 'SELECT v.*, u.user_id, u.username, u.user_colour, u.user_rank, u.user_posts,
+				u.user_avatar, u.user_avatar_type, u.user_avatar_width, u.user_avatar_height
 			FROM ' . USERS_TABLE . ' u
 			LEFT JOIN ' . self::TABLE . ' v ON (v.id = u.user_id)
 			WHERE u.user_id = ' . (int) $id . '
