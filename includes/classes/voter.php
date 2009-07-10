9652c4d6fe9b7424972a0308dad212340a427b90
Chris Smith <chris@cs278.org>
2009-07-10 01:20:08 +0100

    Rename and perform some foo

--- includes/classes/voter.php
+++ includes/classes/voter.php
@@ -1,6 +1,6 @@
 <?php
 
-class vote_user
+class voter
 {
 	private $id;
 	private $name;
@@ -11,59 +11,54 @@ class vote_user
 
 	const DEFAULT_POINTS = 20;
 
-	const TABLE = STABLES_USERS_TABLE;
+	const TABLE = STABLES_VOTERS_TABLE;
 
-	private function __construct($id, $name)
+	private function __construct(array $data)
 	{
-		$this->id = (int) $id;
-		$this->name = $name;
+		$this->id = (int) $data['id'];
+		$this->name = $data['name'];
 
-		$sql = 'SELECT *
-			FROM ' . self::TABLE . '
-			WHERE user_id = ' . $this->id;
-
-		$result = $db->sql_query($sql);
-		$row = $db->sql_fetchrow($result);
-		$db->sql_freeresult($result);
-
-		if (!$row)
-		{
-			$row = array(
-				'user_base_points'	=> self::DEFAULT_POINTS,
-			);
-
-			$sql = 'INSERT INTO ' . STABLES_USERS_TABLE . ' ' . $db->sql_build_ary('INSERT', $row);
-
-			$db->sql_query($sql);
-		}
-
-		$this->base_points = (int) $row['user_base_points'];
+		$this->base_points = (int) $data['base_points'];
 
 		// Calculate on request
 		$this->points = null;
 	}
 
-	public static function create_from_user(user $user)
-	{
-		return new self((int) $user->data['user_id'], $user->data['username']);
-	}
-
 	public static function get($id)
 	{
-		$sql = 'SELECT username
-			FROM ' . USERS_TABLE . '
-			WHERE user_id = ' . (int) $id;
-
+		$sql = 'SELECT v.*, u.user_id, u.username AS name
+			FROM ' . USERS_TABLE . ' u
+			LEFT JOIN ' . self::TABLE . " v ON (v.id = u.user_id)
+			WHERE u.user_id = $id
+				AND u.user_type IN (" . USER_NORMAL . ', ' . USER_FOUNDER . ')';
 		$result = $db->sql_query($sql);
 		$row = $db->sql_fetchrow($result);
 		$db->sql_freeresult($result);
 
-		if (!$row)
+		if (!$row || empty($row['user_id']))
 		{
+			// Either of these mean the phpBB user does not exist
+
 			return null;
 		}
 
-		return new self((int) $id, $row['username']);
+		if (empty($row['id']))
+		{
+			// Voter doesn't exist
+			$sql_ary = array(
+				'id'			=> (int) $row['user_id'],
+				'base_points'	=> (int) self::DEFAULT_POINTS,
+			);
+
+			$sql = 'INSERT INTO ' . self::TABLE . ' ' . $db->sql_build_ary('INSERT', $sql_ary);
+
+			$db->sql_query($sql);
+
+			// Merge in
+			$row = array_merge($row, $sql_ary);
+		}
+
+		return new self($row);
 	}
 
 	public function __get($var)
