d532eb585ece0f9cd62ae2560f0009a91a02b5aa
Chris Smith <chris@cs278.org>
2009-07-11 14:56:58 +0100

    basic support is coming :D

--- post.php
+++ post.php
@@ -14,6 +14,18 @@ require $phpbb_root_path . 'includes/functions_posting.' . $phpEx;
 
 $user->add_lang('posting');
 
+$submit = isset($_POST['post']);
+$preview = isset($_POST['preview']);
+$edit = (request_var('mode', '') === 'edit');
+
+if ($edit && !isset($_REQUEST['i']))
+{
+	// iirc thats a language key
+	trigger_error('INVALID_MODE');
+}
+
+$id = request_var('i', 0);
+
 $bbcode_status	= (bool) $config['allow_bbcode'];
 $smilies_status	= (bool) $config['allow_smilies'];
 $img_status		= $bbcode_status;
@@ -21,13 +33,36 @@ $url_status		= $bbcode_status && $config['allow_post_links'];
 $flash_status	= $bbcode_status && $config['allow_post_flash'];
 $quote_status	= true;
 
+if (isset($_POST['preview']))
+{
+	// Previewing
+	var_dump($_POST); // Cheating for now ;)
+}
+
+if ($submit || $preview)
+{
+	$title			= utf8_normalize_nfc(request_var('title', '', true));
+	$description	= utf8_normalize_nfc(request_var('description', '', true));
+}
+else if ($edit)
+{
+	$idea = idea::get($id);
+
+	$title			= $idea->title;
+	$description	= $idea->description;
+}
+else
+{
+	$title			= '';
+	$description	= '';
+}
+
 $template->assign_vars(array(
 	// Traverse up one more directory as we are in ./style/
 	//'S_EDITOR'	=> $phpbb_root_path . '../styles/prosilver/template/posting_editor.html',
 
-	'S_SMILIES_ALLOWED'		=> $smilies_status,
-	'S_BBCODE_ALLOWED'		=> $bbcode_status,
-	'S_LINKS_ALLOWED'		=> $url_status,
+	'TITLE'					=> $title,
+	'DESCRIPTION'			=> $description,
 
 	'BBCODE_STATUS'			=> sprintf($user->lang['BBCODE_IS_' . (($bbcode_status) ? 'ON' : 'OFF')], '<a href="' . append_sid("{$phpbb_root_path}faq.$phpEx", 'mode=bbcode') . '">', '</a>'),
 	'IMG_STATUS'			=> $user->lang['IMAGES_ARE_' . (($img_status) ? 'ON' : 'OFF')],
@@ -36,6 +71,9 @@ $template->assign_vars(array(
 	'URL_STATUS'			=> $user->lang['URL_IS_' . (($url_status) ? 'ON' : 'OFF')],
 
 	'S_POST_ACTION'			=> append_sid(NP_ROOT_PATH . '/post.' . $phpEx),
+	'S_SMILIES_ALLOWED'		=> $smilies_status,
+	'S_BBCODE_ALLOWED'		=> $bbcode_status,
+	'S_LINKS_ALLOWED'		=> $url_status,
 ));
 
 generate_smilies('inline', false);
