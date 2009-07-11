00cc31ddf391e9f909772902c6f8823f5c9874e0
Chris Smith <chris@cs278.org>
2009-07-11 14:30:29 +0100

    work on posting editor

--- post.php
+++ post.php
@@ -12,13 +12,28 @@ define('NP_ROOT_PATH', dirname(__FILE__));
 require NP_ROOT_PATH . '/includes/bootstrap.php';
 require $phpbb_root_path . 'includes/functions_posting.' . $phpEx;
 
+$user->add_lang('posting');
+
+$bbcode_status	= (bool) $config['allow_bbcode'];
+$smilies_status	= (bool) $config['allow_smilies'];
+$img_status		= $bbcode_status;
+$url_status		= $bbcode_status && $config['allow_post_links'];
+$flash_status	= $bbcode_status && $config['allow_post_flash'];
+$quote_status	= true;
+
 $template->assign_vars(array(
 	// Traverse up one more directory as we are in ./style/
-	'S_EDITOR'	=> $phpbb_root_path . '../styles/prosilver/template/posting_editor.html',
+	//'S_EDITOR'	=> $phpbb_root_path . '../styles/prosilver/template/posting_editor.html',
 
 	'S_SMILIES_ALLOWED'	=> true,
-	'BBCODE_STATUS'		=> true,
 	'S_BBCODE_ALLOWED'	=> true,
+
+	'BBCODE_STATUS'			=> sprintf($user->lang['BBCODE_IS_' . (($bbcode_status) ? 'ON' : 'OFF')], '<a href="' . append_sid("{$phpbb_root_path}faq.$phpEx", 'mode=bbcode') . '">', '</a>'),
+	'IMG_STATUS'			=> $user->lang['IMAGES_ARE_' . (($img_status) ? 'ON' : 'OFF')],
+	'FLASH_STATUS'			=> $user->lang['FLASH_IS_' . (($flash_status) ? 'ON' : 'OFF')],
+	'SMILIES_STATUS'		=> $user->lang['SMILIES_ARE_' . (($smilies_status) ? 'ON' : 'OFF')],
+	'URL_STATUS'			=> $user->lang['URL_IS_' . (($url_status) ? 'ON' : 'OFF')],
+
 ));
 
 generate_smilies('inline', false);
