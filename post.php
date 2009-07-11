03c33b47bd728e669794ed96f5f279e0fa802ef2
Chris Smith <chris@cs278.org>
2009-07-11 13:53:57 +0100

    Smilies

--- post.php
+++ post.php
@@ -10,11 +10,14 @@ define('NP_ROOT_PATH', dirname(__FILE__));
 
 // Bootstrap
 require NP_ROOT_PATH . '/includes/bootstrap.php';
+require $phpbb_root_path . 'includes/functions_posting.' . $phpEx;
 
 $template->assign_vars(array(
 	// Traverse up one more directory as we are in ./style/
 	'S_EDITOR'	=> $phpbb_root_path . '../styles/prosilver/template/posting_editor.html',
 ));
+
+generate_smilies('inline');
 
 // Return control to phpBB
 np_unregister();
