bf07d787e8d1a857de97dc55d52aeb9db30d602b
Chris Smith <chris@cs278.org>
2009-07-12 19:31:13 +0100

    style vars and changes

--- index.php
+++ index.php
@@ -25,8 +25,14 @@ foreach (idea::find(idea::POPULAR, 25) as $idea)
 		'USERNAME'		=> $idea->user->username('full'),
 		'SCORE'			=> $idea->score,
 
-		'U_VOTE_UP'		=> append_sid(NP_ROOT_PATH . '/vote.' . $phpEx, "i={$idea->id}&amp;up&amp;place"),
-		'U_VOTE_DOWN'	=> append_sid(NP_ROOT_PATH . '/vote.' . $phpEx, "i={$idea->id}&amp;down&amp;place"),
+		'S_CAN_DELETE'	=> $idea->can_delete(),
+		'S_CAN_EDIT'	=> $idea->can_edit(),
+		'S_CAN_VOTE'	=> $idea->can_vote(),
+
+		'U_DELETE'		=> ($this->can_delete()) ? append_sid(NP_ROOT_PATH . '/post.' . $phpEx, "i={$idea->id}&delete") : false,
+		'U_EDIT'		=> ($idea->can_edit()) ? append_sid(NP_ROOT_PATH . '/post.' . $phpEx, "i={$idea->id}") : false,
+		'U_VOTE_UP'		=> ($idea->can_vote()) ? append_sid(NP_ROOT_PATH . '/vote.' . $phpEx, "i={$idea->id}&amp;up&amp;place") : false,
+		'U_VOTE_DOWN'	=> ($idea->can_vote()) ? append_sid(NP_ROOT_PATH . '/vote.' . $phpEx, "i={$idea->id}&amp;down&amp;place") : false,
 	), array_combine(array('RANK_TITLE', 'RANK_IMG', 'RANK_IMG_SRC'), $idea->user->rank())));
 }
 
