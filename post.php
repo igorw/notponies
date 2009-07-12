ef86f591d1188cd9d2f92e6d937ef84bf4c61704
Chris Smith <chris@cs278.org>
2009-07-12 19:21:29 +0100

    bah

--- post.php
+++ post.php
@@ -107,7 +107,7 @@ $template->assign_vars(array(
 	'SMILIES_STATUS'		=> $user->lang['SMILIES_ARE_' . (($smilies_status) ? 'ON' : 'OFF')],
 	'URL_STATUS'			=> $user->lang['URL_IS_' . (($url_status) ? 'ON' : 'OFF')],
 
-	'S_POST_ACTION'			=> append_sid(NP_ROOT_PATH . '/post.' . $phpEx, (!empty($params) ? implode('&', $params) : false)),
+	'S_POST_ACTION'			=> append_sid(NP_ROOT_PATH . '/post.' . $phpEx, (!empty($params) ? implode('&amp;', $params) : false)),
 	'S_SMILIES_ALLOWED'		=> $smilies_status,
 	'S_BBCODE_ALLOWED'		=> $bbcode_status,
 	'S_LINKS_ALLOWED'		=> $url_status,
