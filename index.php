1065472e83546bb93412608e64b3ed462d62f894
Chris Smith <chris@cs278.org>
2009-07-14 13:48:01 +0100

    sorting, change vote auth checks

--- index.php
+++ index.php
@@ -11,13 +11,18 @@ define('NP_ROOT_PATH', '.');
 // Bootstrap
 require NP_ROOT_PATH . '/includes/bootstrap.php';
 
+// @todo Order
+$sort = request_var('sort', 'score');
+
 // Business logic
 $template->assign_vars(array(
-	'S_CAN_CREATE'	=> idea::can_create(),
+	'S_CAN_CREATE'		=> idea::can_create(),
 
-	'U_CREATE'		=> (idea::can_create()) ? append_sid(NP_ROOT_PATH . '/post.' . $phpEx) : false,
+	'U_CREATE'			=> (idea::can_create()) ? append_sid(NP_ROOT_PATH . '/post.' . $phpEx) : false,
 
-	'U_POST_IDEA'	=> append_sid(NP_ROOT_PATH . '/post.' . $phpEx),
+	'U_SORT_NEWEST'		=> append_sid(NP_ROOT_PATH . '/index.' . $phpEx, 'sort=newest'),
+	'U_SORT_POPULAR'	=> append_sid(NP_ROOT_PATH . '/index.' . $phpEx, 'sort=popular'),
+	'U_SORT_SCORE'		=> append_sid(NP_ROOT_PATH . '/index.' . $phpEx, 'sort=score'),
 ));
 
 foreach (idea::find(idea::POPULAR, 25) as $idea)
@@ -32,13 +37,13 @@ foreach (idea::find(idea::POPULAR, 25) as $idea)
 		'S_CAN_DELETE'		=> $idea->can_delete(),
 		'S_CAN_EDIT'		=> $idea->can_edit(),
 		'S_CAN_VOTE'		=> $idea->can_vote(),
-		'S_CAN_VOTE_UP'		=> ($idea->can_vote() || ($idea->can_vote_change() && $idea->get_vote()->value === vote::DOWN)),
-		'S_CAN_VOTE_DOWN'	=> ($idea->can_vote() || ($idea->can_vote_change() && $idea->get_vote()->value === vote::UP)),
+		'S_CAN_VOTE_UP'		=> ($idea->can_vote() || $idea->can_vote_change(vote::UP)),
+		'S_CAN_VOTE_DOWN'	=> ($idea->can_vote() || $idea->can_vote_change(vote::DOWN)),
 
 		'U_DELETE'		=> ($idea->can_delete()) ? append_sid(NP_ROOT_PATH . '/post.' . $phpEx, "i={$idea->id}&delete") : false,
 		'U_EDIT'		=> ($idea->can_edit()) ? append_sid(NP_ROOT_PATH . '/post.' . $phpEx, "i={$idea->id}") : false,
-		'U_VOTE_UP'		=> ($idea->can_vote() || ($idea->can_vote_change() && $idea->get_vote()->value === vote::DOWN)) ? append_sid(NP_ROOT_PATH . '/vote.' . $phpEx, "i={$idea->id}&amp;up&amp;place") : false,
-		'U_VOTE_DOWN'	=> ($idea->can_vote() || ($idea->can_vote_change() && $idea->get_vote()->value === vote::UP)) ? append_sid(NP_ROOT_PATH . '/vote.' . $phpEx, "i={$idea->id}&amp;down&amp;place") : false,
+		'U_VOTE_UP'		=> ($idea->can_vote() || $idea->can_vote_change(vote::UP)) ? append_sid(NP_ROOT_PATH . '/vote.' . $phpEx, "i={$idea->id}&amp;up&amp;place") : false,
+		'U_VOTE_DOWN'	=> ($idea->can_vote() || $idea->can_vote_change(vote::DOWN)) ? append_sid(NP_ROOT_PATH . '/vote.' . $phpEx, "i={$idea->id}&amp;down&amp;place") : false,
 	), array_combine(array('RANK_TITLE', 'RANK_IMG', 'RANK_IMG_SRC'), $idea->user->rank())));
 }
 
